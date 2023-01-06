9438b36c77a90243c1054098845058ec21d3c157
samanismail <mymaket4321@hotmail.com>
2023-01-06 12:28:36 +0100

    Algo djikastra, chemin, percer murs

--- Grille.py
+++ Grille.py
@@ -8,7 +8,6 @@ class Grille:
     def __init__(self, lignes, colonnes):
         self.lignes = lignes
         self.colonnes = colonnes
-
         self.grille = self.creationGrille()
 
     def getLignes(self):
@@ -53,14 +52,12 @@ class Grille:
                 grille[i][j].setDroit(epaisseurs[random.randint(0, 4)])
                 grille[i][j].setBas(epaisseurs[random.randint(0, 4)])
                 grille[i][j].setCentre(i, j)
-        for i in range(self.lignes):
-            for j in range(self.colonnes):
-                print(grille[i][j])
         return grille
 
     def AfficherGrille(self):
         plt.figure()
         plt.axis('off')
+        plt.title("Grille initiale")
         for i in range(len(self.grille)):
             for element in self.grille[i]:
                 ordonne, abscisse = -element.getCentre()[0], element.getCentre()[1]
@@ -80,42 +77,140 @@ class Grille:
 
     def creationDictionnaire(self):
         dico = {}
-        for i in range(self.lignes):
-            for j in range(self.colonnes):
-                L=[]
-                if i == j == 0:
-                    L.append([(i, j + 1), self.grille[i][j].getDroit()])
-                    L.append([(i + 1, j), self.grille[i][j].getBas()])
-                elif i == 0 and j == self.colonnes - 1:
-                    L.append([(i, j - 1), self.grille[i][j].getGauche()])
-                    L.append([(i + 1, j), self.grille[i][j].getBas()])
-                elif i == self.lignes - 1 and j == 0:
-                    L.append([(i, j + 1), self.grille[i][j].getDroit()])
-                    L.append([(i - 1, j), self.grille[i][j].getHaut()])
-                elif i == self.lignes - 1 and j == self.colonnes - 1:
-                    L.append([(i, j - 1), self.grille[i][j].getGauche()])
-                    L.append([(i - 1, j), self.grille[i][j].getHaut()])
-                elif i == 0 and 1 < j < self.colonnes - 2:
-                    L.append([(i, j + 1), self.grille[i][j].getDroit()])
-                    L.append([(i, j - 1), self.grille[i][j].getGauche()])
-                    L.append([(i + 1, j), self.grille[i][j].getBas()])
-                elif i == self.lignes - 1 and 1 < j < self.colonnes - 2:
-                    L.append([(i, j + 1), self.grille[i][j].getDroit()])
-                    L.append([(i, j - 1), self.grille[i][j].getGauche()])
-                    L.append([(i - 1, j), self.grille[i][j].getHaut()])
-                elif j == 0 and 1 < i < self.lignes - 2:
-                    L.append([(i + 1, j), self.grille[i][j].getBas()])
-                    L.append([(i - 1, j), self.grille[i][j].getHaut()])
-                    L.append([(i, j + 1), self.grille[i][j].getDroit()])
-                elif j == self.colonnes - 1 and 1 < i < self.lignes - 2:
-                    L.append([(i + 1, j), self.grille[i][j].getBas()])
-                    L.append([(i - 1, j), self.grille[i][j].getHaut()])
-                    L.append([(i, j - 1), self.grille[i][j].getGauche()])
-                else:
-                    L.append([(i + 1, j), self.grille[i][j].getBas()])
-                    L.append([(i - 1, j), self.grille[i][j].getHaut()])
-                    L.append([(i, j + 1), self.grille[i][j].getDroit()])
-                    L.append([(i, j - 1), self.grille[i][j].getGauche()])
-                dico[(i, j)] = L
+        compteur = 0
+        for i in range(self.lignes * self.colonnes):
+            L = []
+            if compteur == 0:
+                L.append((i + 1, self.grille[compteur // self.colonnes][compteur % self.colonnes].getDroit()))
+                L.append((i + self.colonnes, self.grille[compteur // self.colonnes][compteur % self.colonnes].getBas()))
+            elif compteur == self.colonnes - 1:
+                L.append((i - 1, self.grille[compteur // self.colonnes][compteur % self.colonnes].getGauche()))
+                L.append((i + self.colonnes, self.grille[compteur // self.colonnes][compteur % self.colonnes].getBas()))
+            elif compteur == self.lignes * self.colonnes - self.colonnes:
+                L.append(
+                    (i - self.colonnes, self.grille[compteur // self.colonnes][compteur % self.colonnes].getHaut()))
+                L.append((i + 1, self.grille[compteur // self.colonnes][compteur % self.colonnes].getDroit()))
+            elif compteur == self.lignes * self.colonnes - 1:
+                L.append(
+                    (i - self.colonnes, self.grille[compteur // self.colonnes][compteur % self.colonnes].getHaut()))
+                L.append((i - 1, self.grille[compteur // self.colonnes][compteur % self.colonnes].getGauche()))
+            elif compteur < self.colonnes:
+                L.append((i - 1, self.grille[compteur // self.colonnes][compteur % self.colonnes].getGauche()))
+                L.append((i + 1, self.grille[compteur // self.colonnes][compteur % self.colonnes].getDroit()))
+                L.append((i + self.colonnes, self.grille[compteur // self.colonnes][compteur % self.colonnes].getBas()))
+            elif compteur % self.colonnes == 0:
+                L.append(
+                    (i - self.colonnes, self.grille[compteur // self.colonnes][compteur % self.colonnes].getHaut()))
+                L.append((i + 1, self.grille[compteur // self.colonnes][compteur % self.colonnes].getDroit()))
+                L.append((i + self.colonnes, self.grille[compteur // self.colonnes][compteur % self.colonnes].getBas()))
+            elif compteur % self.colonnes == self.colonnes - 1:
+                L.append(
+                    (i - self.colonnes, self.grille[compteur // self.colonnes][compteur % self.colonnes].getHaut()))
+                L.append((i - 1, self.grille[compteur // self.colonnes][compteur % self.colonnes].getGauche()))
+                L.append((i + self.colonnes, self.grille[compteur // self.colonnes][compteur % self.colonnes].getBas()))
+            elif compteur > self.lignes * self.colonnes - self.colonnes:
+                L.append(
+                    (i - self.colonnes, self.grille[compteur // self.colonnes][compteur % self.colonnes].getHaut()))
+                L.append((i - 1, self.grille[compteur // self.colonnes][compteur % self.colonnes].getGauche()))
+                L.append((i + 1, self.grille[compteur // self.colonnes][compteur % self.colonnes].getDroit()))
+            else:
+                L.append(
+                    (i - self.colonnes, self.grille[compteur // self.colonnes][compteur % self.colonnes].getHaut()))
+                L.append((i - 1, self.grille[compteur // self.colonnes][compteur % self.colonnes].getGauche()))
+                L.append((i + 1, self.grille[compteur // self.colonnes][compteur % self.colonnes].getDroit()))
+                L.append((i + self.colonnes, self.grille[compteur // self.colonnes][compteur % self.colonnes].getBas()))
 
+            dico[i] = L
+            compteur += 1
         return dico
+
+    def dijkstra_pred(self):
+        G = self.creationDictionnaire()
+        s = 0
+        D = {}  # tableau final des distances minimales
+        d = {k: float('inf') for k in G}  # distances initiales infinies
+        d[s] = 0  # sommet de départ
+        P = {}  # liste des prédécesseurs
+        while len(d) > 0:  # fin quand d est vide
+            k = self.minimum(d)  # sommet de distance minimale pour démarrer une étape
+            for i in range(len(G[k])):  # on parcourt les voisins de k
+                v, c = G[k][i]  # v voisin de k, c la distance à k
+                if v not in D:  # si v n'a pas été déjà traité
+                    if d[v] > d[k] + c:  # est-ce plus court en passant par k ?
+                        d[v] = d[k] + c
+                        P[v] = k  # stockage du prédécesseur de v
+            D[k] = d[k]  # copie du sommet et de la distance dans D
+            del (d[k])  # suppression du sommet de d
+        return D, P  # on retourne aussi la liste des prédécesseurs
+
+    def minimum(self, dico):
+        i = -1
+        m = float('inf')
+        for k in dico:  # parcours des clés
+            if dico[k] < m:
+                m = dico[k]
+                i = k
+        return i
+
+    def chemin(self):
+        D, P = self.dijkstra_pred()
+        i = self.lignes * self.colonnes - 1
+        chemin = [(i // self.colonnes, i % self.colonnes)]
+        while i in P:
+            i = P[i]
+            chemin.append((i // self.colonnes, i % self.colonnes))
+        chemin.reverse()
+        return chemin
+
+    def afficherChemin(self):
+        plt.axis("off")
+        plt.title("Grille percéé")
+        chemin = self.chemin()
+        plt.plot([0, 0], [0.5, 0], color='brown', linewidth=10)
+        plt.plot([self.lignes - 1, self.lignes - 1], [-self.lignes + 1, -self.lignes + 0.5], color='brown',
+                 linewidth=10)
+        for i in range(len(chemin) - 1):
+            plt.plot([chemin[i][1], chemin[i + 1][1]], [-chemin[i][0], -chemin[i + 1][0]], color='brown', linewidth=10)
+        # percer des murs afin d'obtenir le chemin voulu.
+        for i in range(len(chemin) - 1):
+            # cellule gauche haut
+            if i == 0:
+                self.grille[0][0].setHaut(0)
+            # cellule doite bas
+            if i == len(chemin) - 2:
+                self.grille[self.lignes - 1][self.colonnes - 1].setBas(0)
+            if chemin[i][0] == chemin[i + 1][0]:
+                if chemin[i][1] < chemin[i + 1][1]:
+                    self.grille[chemin[i][0]][chemin[i][1]].setDroit(0)
+                    self.grille[chemin[i + 1][0]][chemin[i + 1][1]].setGauche(0)
+                else:
+                    self.grille[chemin[i][0]][chemin[i][1]].setGauche(0)
+                    self.grille[chemin[i + 1][0]][chemin[i + 1][1]].setDroit(0)
+            else:
+                if chemin[i][0] < chemin[i + 1][0]:
+                    self.grille[chemin[i][0]][chemin[i][1]].setBas(0)
+                    self.grille[chemin[i + 1][0]][chemin[i + 1][1]].setHaut(0)
+                else:
+                    self.grille[chemin[i][0]][chemin[i][1]].setHaut(0)
+                    self.grille[chemin[i + 1][0]][chemin[i + 1][1]].setBas(0)
+        for i in range(len(self.grille)):
+            for element in self.grille[i]:
+                ordonne, abscisse = -element.getCentre()[0], element.getCentre()[1]
+                if not element.getGauche() == 0:
+                    plt.plot([abscisse - 0.5, abscisse - 0.5], [ordonne - 0.5, ordonne + 0.5], color='aqua',
+                             linewidth=element.getGauche())
+                if not element.getHaut() == 0:
+                    plt.plot([abscisse - 0.5, abscisse + 0.5], [ordonne + 0.5, ordonne + 0.5], color='aqua',
+                             linewidth=element.getHaut())
+                if not element.getDroit() == 0:
+                    plt.plot([abscisse + 0.5, abscisse + 0.5], [ordonne - 0.5, ordonne + 0.5], color='aqua',
+                             linewidth=element.getDroit())
+                if not element.getBas() == 0:
+                    plt.plot([abscisse - 0.5, abscisse + 0.5], [ordonne - 0.5, ordonne - 0.5], color='aqua',
+                             linewidth=element.getBas())
+
+        plt.show()
+
+    def calculCout(self):
+        D, P = self.dijkstra_pred()
+        return D[self.lignes * self.colonnes - 1]
--- Main.py
+++ Main.py
@@ -1,4 +1,12 @@
 from Grille import Grille
-grille = Grille(3, 3)
+n=-1
+m=-1
+while n<0 or type(n) != int:
+    n = int(input("Entrer le nombre de lignes: "))
+while m<0 or type(m) != int:
+    m = int(input("Entrer le nombre de colonnes: "))
+grille = Grille(n, m)
 grille.AfficherGrille()
-print(grille.creationDictionnaire())
+grille.afficherChemin()
+print("cout: " + str(grille.calculCout()))
+print("Chemin: " + str(grille.chemin()))
